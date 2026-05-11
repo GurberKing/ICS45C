80027751fc4f3256a3b611afd5c0066722a4c19d
junhyeok <pjh9095@gmail.com>
2026-05-10 17:23:10 -0700

    Switch Statement (Python)

--- readingweek1/readingweek1.py
+++ readingweek1/readingweek1.py
@@ -1,4 +1,19 @@
-def main():
-    print("Hello World")
+def gradeGenerator():
+    score = int(input("Enter a student score: "))
+    condition = int(score / 10)
 
-main()
+    match condition:
+        case 10:
+            print("A")
+        case 9:
+            print("A")
+        case 8:
+            print("B")
+        case 7:
+            print("C")
+        case 6:
+            print("D")
+        case _:
+            print("F")
+
+gradeGenerator()

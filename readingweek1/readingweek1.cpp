3e49753fbd2e789782aea3949f2fe6a60eddf816
JUNHYEOK <junhyep4@uci.edu>
2026-05-11 15:47:25 -0700

    ++i, i++

--- readingweek1/readingweek1.cpp
+++ readingweek1/readingweek1.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main(){
-    int score;
-    cout << "Enter a student score: ";
-    cin >> score;
-
-    int condition = score / 10;
-    switch(condition) {
-        case 10:
-        case 9:
-            cout << "A" << endl;
-            break;
-        case 8:
-            cout << "B" << endl;
-            break;
-        case 7:
-            cout << "C" << endl;
-            break;
-        case 6:
-            cout << "D" << endl;
-            break;
-        default:
-            cout << "F" << endl;
+    for (int i = 0; i < 10; i++) {  // ++i 나 i++ 나 loop 안에서는 0부터 9까지임 i값
+        cout << i * i << endl;
     }
-
     return 0;
 } 

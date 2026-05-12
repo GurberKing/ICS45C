dbf86167879b82782a47baa95d4ecffad2b9f61e
JUNHYEOK <junhyep4@uci.edu>
2026-05-11 17:56:04 -0700

    Python Array Parameter with List

--- readingweek1/readingweek1.py
+++ readingweek1/readingweek1.py
@@ -1,11 +1,14 @@
-def swap(val1, val2):
-    return val2, val1
+# 파이썬은 c++과는 다르게 매개변수안에 자신의 길이를 안넘겨줘도 됨.
 
-num1 = 2
-num2 = 3
+def average(lst):
+    total = 0.0
+    for count in range(len(lst)):
+        total += float(lst[count])
+    
+    return total / len(lst)
 
-num1, num2 = swap(num1, num2)
-print(num1, num2)
+def average2(lst):
+    return sum(lst)/len(lst)
 
-num1, num2 = num2, num1
-print(num1, num2)
+print(average([10,20,30,40,50]))
+print(average2([10,20,30,40,50]))

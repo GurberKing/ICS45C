f5f7f295091c5f1a5fe1126fde3b7eedee0b7b35
Junhyeok Park <JUNHYEP4@uci.edu>
2026-05-11 19:28:33 -0700

    Function Overloading(Python)

--- readingweek1/readingweek1.py
+++ readingweek1/readingweek1.py
@@ -1,14 +1,9 @@
-# 파이썬은 c++과는 다르게 매개변수안에 자신의 길이를 안넘겨줘도 됨.
+def overload(n, m=None):
+    if m is None:
+        print("Value: ", n)
+    else:
+        print("1st Value: ", n)
+        print("2nd Value: ", m)
 
-def average(lst):
-    total = 0.0
-    for count in range(len(lst)):
-        total += float(lst[count])
-    
-    return total / len(lst)
-
-def average2(lst):
-    return sum(lst)/len(lst)
-
-print(average([10,20,30,40,50]))
-print(average2([10,20,30,40,50]))
+overload(1)
+overload(2, 3)

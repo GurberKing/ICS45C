33d8abdc0000c7730d568caa201e902bf4aa8631
Junhyeok Park <JUNHYEP4@uci.edu>
2026-05-12 15:07:03 -0700

    c++ array out of range disadvantages

--- readingweek1/readingweek1.cpp
+++ readingweek1/readingweek1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int arr1[] = {1, 2, 3, 4}; // static allocation
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 8; ++i) {  // c++ 은 python 과는 다르게 인덱스가 초과해도 무조건 실행하려함! 빠르나 치명적인 단점!
         cout << arr1[i] << endl;
     }
 

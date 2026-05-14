eeae5312f3509bb8485ac08d2b3ca4230e40112c
Junhyeok Park <JUNHYEP4@uci.edu>
2026-05-14 13:47:37 -0700

    c++ array outbound potentially changed

--- readingweek1/readingweek1.cpp
+++ readingweek1/readingweek1.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-int main() {
-    int arr1[] = {1, 2, 3, 4}; // static allocation
-    for (int i = 0; i < 8; ++i) {  // c++ 은 python 과는 다르게 인덱스가 초과해도 무조건 실행하려함! 빠르나 치명적인 단점!
-        cout << arr1[i] << endl;
+int main(){
+    int myarray[] = {2, 4};
+    int otherdata[]={777, 777};
+    for (int i=0; i<4; i++){
+        myarray[i]=0;
+        cout <<"myarray["<< i << "]=";
+        cout << myarray[i]<< endl;
+        cout << "add:" << &myarray[i] << endl;
+    }
+    // c++ 에서는 첫번째 어레이의 범위를 벗어나면 다른 어레이의 값이 변경될 수 있다. 여기 vscode에서는 그렇지 않지만. 그럴 확률이 있다는 뜻인듯.
+    for (int i=0; i<2; i++){
+        cout <<"otherdata["<< i << "]=";
+        cout << otherdata[i]<< endl;
+        cout << "add:" << &otherdata[i] << endl;
     }
 
     return 0;

2b1b8421249385a73b2af1397319fbcf3d5eb7a3
Junhyeok Park <JUNHYEP4@uci.edu>
2026-05-14 21:48:07 -0700

    Automating Test

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -14,11 +14,20 @@ bool isVowel(char c) {
     }
 }
 
-void testVowel() {
-    cout << "a: " << isVowel('a') << endl;
-    cout << "b: " << isVowel('b') << endl;
+// 테스트 자동화
+// 만약에 'a'를 isVowel에 넣었는데 에러가 발생하면 잘못된거니깐 1번이 잘못됐다고 알려줌
+// 그러고 나서 'b'를 넣었는데 에러가 발생하면 잘못된거니깐 2번이 잘못됐다고 알려줌.
+int testVowel() {
+    if (!isVowel('a')) return 1;
+    if (isVowel('b')) return 2;
+    return 0;
 }
 
 int main() {
+    int result { testVowel() };
+    if (result != 0)
+        cout << "testVowel() test " << result << "failed" << endl;
+    else
+        cout << "testVowel() tests passed" << endl;
     return 0;
 }

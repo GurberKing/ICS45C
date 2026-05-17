a008150a7079c180fce222367f945c554e8824c5
Junhyeok Park <JUNHYEP4@uci.edu>
2026-05-17 14:54:09 -0700

    initialize constant value by non-constant variable

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -7,7 +7,12 @@ const double gravity { 9.8 }; // 상수변수 Const Variable
 int const sidesInSquare { 4}; // east const style
 
 int main() {
+    int age;
     cout << "gravity: " << gravity << '\n';
     cout << "SidesInSquare: " << sidesInSquare << '\n';
+    cout << "Enter a age: ";
+    cin >> age;
+    const int constage {age};
+    cout << "const age is: " << constage << '\n'; // const variable 은 비상수변수 non-constant age 로도 초기화가능
     return 0;
 }

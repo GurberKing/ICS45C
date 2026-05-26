db5a42ae87c39d20570ac30f277a711d144047b1
Junhyeok Park <JUNHYEP4@uci.edu>
2026-05-25 23:59:15 -0700

    cout cin 다중입력

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <string>
-using namespace std::literals; // s와 sv 접미사 접근위해
-
-#include <string_view>
+using namespace std;
 int main() {
-    auto d { 5.0 };
-    auto i { 1 + 2 };
-    auto x { i };
-    auto a { 1.23f };
-    auto b { 5u };
+    int num1 {};
+    int den1 {};
 
-    int c { 5 };
-    const auto b { 5 };
-    constexpr auto c { 5 }; // const 처럼 변경 불가 + 컴파일 시 바로 계산되게끔. 즉 숫자 상수, 배열크기, 수학공식처럼 컴파일전에 미리 알 수 있는값들.
-    auto s1 { "goo"s };
-    auto s2 { "moo"sv };
+    int num2 {};
+    int den2 {};
+
+    char ignore {};
+
+    cout << "Enter a 1 fraction: ";
+    cin >> num1 >> ignore >> den1;
+
+    cout << "Enter a 2 fraction: ";
+    cin >> num2 >> ignore >> den2;
+
+    cout << "The two fractions multiplied: "
+    << num1 * num2 << '/' << den1 * den2 << '\n';
     return 0;
 }

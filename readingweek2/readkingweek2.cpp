bee2ec612af5990d11459798c878341d79f9e123
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 15:05:17 +0900

    12.4 constexpr 참조 정의 시, constexpr과 const 모두 적용해야함. constexpr 은 참조 자체에 적용, const는 참조되는 자료형에 적용.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -4,13 +4,8 @@ using namespace std;
 
 int g_x { 5 };
 int main() {
-    constexpr int& ref1 { g_x }; // 가능: 전역변수에 바인딩
+    static const int s_x { 6 }; // const int 이다.
 
-    static int s_x { 6 };
-    constexpr int& ref2 { s_x }; // 가능: 정적 지역 변수에 바인딩
-
-    int x { 6 };
-    constexpr int& ref3 { x }; // 오류: 정적 저장 기간을 갖지 않는 객체에는 바인딩 불가.
-
+    [[maybe_unused]] constexpr const int& ref2 {s_x};// constexpr과 const 둘 다 필요.
     return 0;
 }

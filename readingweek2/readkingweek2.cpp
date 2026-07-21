a2ab71a83a1f39809e44d40e37044138ec2d155e
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 12:46:38 +0900

    12.3 lvalue reference, 여기서 &는 pointer의 주소가 아니라 lvalue에 대한 참조를 뜻함.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,12 +2,10 @@
 #include <string>
 using namespace std;
 int main() {
-    int x = 10;
-    int& r = x; // lvalue 는 이거 가능
-    r = 20;
+    int x { 5 };
+    int& ref { x };
     cout << x << endl;
+    cout << ref << endl;
 
-    int& s = 5; // rvalue 는 불가
-    const int& t = 5; // const 는 lvalue rvalue 다 가능
     return 0;
 }

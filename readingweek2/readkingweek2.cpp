73c65ce8ced0774e1e0d49b482e2e3aac66c224a
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 16:00:47 +0900

    12.5 passbyreference는 매개변수 변경 시 실제 인수 값도 변경.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,13 +2,13 @@
 #include <string>
 using namespace std;
 
-void addOne(int y) { //y는 x의 복사본이다.
-    ++y; // 실제 x가 아니라 x의 복사본을 변경한다.
+void addOne(int& y) { //y는 실제 객체 x에 바인딩 된다.
+    ++y; // 실제 x를 변경
 }
 int main() {
     int x { 5 };
     cout << "value: " << x << '\n';
     addOne(x);
-    cout << "value: " << x << '\n'; // x는 변경되지 않음.
+    cout << "value: " << x << '\n'; // x는 6으로 변경.
     return 0;
 }

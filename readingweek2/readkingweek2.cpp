b2f27f3100f5a0ef2ed8dbd4c37a5a96f824c8a5
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 13:02:49 +0900

    12.3 비상수 lvalue 참조시 const로 초기화되어있는 변수랑 rvalue랑은 바인딩 불가

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
     int x { 5 };
     int& ref { x };
-    cout << x << ref << endl;
-
-    x = 6 ;
-    cout << x << ref << endl;
     
-    ref = 7;
-    cout << x << ref << endl;
+    const int y {5};
+    int& invalidRef { y }; // 비상수 Lvalue 참조는 수정할 수 없는 Lvalue에 바인딩 불가
+
+    int& invalidRef2 { 0 }; // 비상수 Lvalue 참조는 rvalue에 바인딩 불가
     return 0;
 }

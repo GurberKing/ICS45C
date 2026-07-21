fdac4f3753f8e3b01b91c35f867a192343454341
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 15:39:07 +0900

    pass by reference 는 복사를 할 필요가 없기 때문에 더 효율적이다.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,11 +2,11 @@
 #include <string>
 using namespace std;
 
-void printValue(std::string y) {
+void printValue(std::string& y) { // 자료형이 string&  참조로 변환.
     cout << y << '\n';
 } // 여기서 y 소멸
 int main() {
     string x { "Hello World!" }; // x 는 std::string 이다.
-    printValue(x); // x 가 값으로 전달되어 y로 복사된다. 비용 큼.
+    printValue(x); // x 가 참조 배개변수 y로 참조 전달. 비용이 적음.
     return 0;
 }

e8e79732a96d074d2ea45d85489bb5044309678e
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 16:06:11 +0900

    12.5 pass by reference 는 수정 가능 한 lvalue만 가능. const 참조와 rvalue는 불가.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,13 +2,16 @@
 #include <string>
 using namespace std;
 
-void addOne(int& y) { //y는 실제 객체 x에 바인딩 된다.
-    ++y; // 실제 x를 변경
+void printValue(int& y) { // y는 수정 가능한 lvalue만 받을 수 있다.
+    cout << y << '\n';
 }
 int main() {
     int x { 5 };
-    cout << "value: " << x << '\n';
-    addOne(x);
-    cout << "value: " << x << '\n'; // x는 6으로 변경.
+    printValue(x); // 정상: x는 수정 가능한 lvalue
+    
+    const int z { 5 };
+    printValue(z); // 오류: z는 수정할 수 없는 lvalue
+
+    printValue(5); // 오류:5는 rvalue
     return 0;
 }

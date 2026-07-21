335f299ad146922e9aedb7669428dfdad205dd70
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 17:05:35 +0900

    12.6 passbyvalue, passbyreference, passbyconstreference 동시 사용 함수.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,16 +2,12 @@
 #include <string>
 using namespace std;
 
-void printRef(const int& y) { // y는 상수 참조 const reference
-    cout << y << '\n';
+void foo(int a, int& b, const string& c) {
 }
 int main() {
     int x { 5 };
-    printRef(x); // 정상: x는 수정 가능한 lvalue
+    string s { "Hello World!" };
     
-    const int z { 5 };
-    printRef(z); // 가능: x는 수정 가능한 lvalue, y 는 z에 바인딩
-
-    printRef(5); // 가능: 5는 rvalue, y 는 임시 int 객체에  바인딩.
+    foo(5, x, s);
     return 0;
 }

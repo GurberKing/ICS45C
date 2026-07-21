8b584715982b812058e942cfe4dd2768b2438bd5
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 14:31:07 +0900

    12.4 다른 자료형의 값으로 const lvalue 참조 초기화

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,7 +2,13 @@
 #include <string>
 using namespace std;
 int main() {
-    const int& ref { 5 }; // const에 대한 lvalue 참조는 rvalue에도 바인딩 가능.
-    cout << ref << endl;
+    const double& r1 { 5 }; // 값 int 5로 임시 double 객체가 초기화, 
+    cout << r1 << endl;
+
+    char c { 'a' };
+    const int& r2 { c }; // int 자료형의 임시객체가 생성되고, char 'a' 로 초기화
+    cout << r2 << endl; // r2가 int에 대한 참조이기 때문에 97 출력
+
+    // 두 개 모두 참조의 자료형과 임시 객체의 자료형은 동일
     return 0;
 }

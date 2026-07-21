d923593eaa87d6936e95ce40ba1d92f06fbd6ae6
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 11:27:48 +0900

    Test Hello World

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,21 +2,6 @@
 #include <string>
 using namespace std;
 int main() {
-    int num1 {};
-    int den1 {};
-
-    int num2 {};
-    int den2 {};
-
-    char ignore {};
-
-    cout << "Enter a 1 fraction: ";
-    cin >> num1 >> ignore >> den1;
-
-    cout << "Enter a 2 fraction: ";
-    cin >> num2 >> ignore >> den2;
-
-    cout << "The two fractions multiplied: "
-    << num1 * num2 << '/' << den1 * den2 << '\n';
+    cout << "Hello World!" << endl;
     return 0;
 }

ff764e7f8b8a3f7710974bd3ec08424a9029e977
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 14:11:25 +0900

    12.4 const에 대한 참조는 무조건 둘 다 const 사용

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,12 +2,7 @@
 #include <string>
 using namespace std;
 int main() {
-    int x { 5 };
-    int& ref { x };
-    
-    const int y {5};
-    int& invalidRef { y }; // 비상수 Lvalue 참조는 수정할 수 없는 Lvalue에 바인딩 불가
-
-    int& invalidRef2 { 0 }; // 비상수 Lvalue 참조는 rvalue에 바인딩 불가
-    return 0;
+    const int x { 5 };
+    const int& ref { x };
+    cout << ref << endl;
 }

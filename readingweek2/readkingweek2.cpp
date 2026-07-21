f5913f560b32c4f0629c9ba8e07605c38a8a1563
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 16:45:13 +0900

    12.6 pass by const reference 는 어떤 인수에도 바인딩 가능하다. 인수의 복사본을 안만들어도 되니깐 효율적. 함수가 인수의 값을 변경은 못함 const이기에.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -2,16 +2,16 @@
 #include <string>
 using namespace std;
 
-void printValue(int& y) { // y는 수정 가능한 lvalue만 받을 수 있다.
+void printRef(const int& y) { // y는 상수 참조 const reference
     cout << y << '\n';
 }
 int main() {
     int x { 5 };
-    printValue(x); // 정상: x는 수정 가능한 lvalue
+    printRef(x); // 정상: x는 수정 가능한 lvalue
     
     const int z { 5 };
-    printValue(z); // 오류: z는 수정할 수 없는 lvalue
+    printRef(z); // 가능: x는 수정 가능한 lvalue, y 는 z에 바인딩
 
-    printValue(5); // 오류:5는 rvalue
+    printRef(5); // 가능: 5는 rvalue, y 는 임시 int 객체에  바인딩.
     return 0;
 }

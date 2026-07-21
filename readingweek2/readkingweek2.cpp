7e444f440c8039c94eb7e8c1d4cc164d69a83e7c
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 14:57:32 +0900

    12.4 constexpr 참조에는 전역변수나 정적 지역변수에  가능. 즉, 정적저장기간(static duration)을 가진 객체에만 바인딩 가능.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int g_x { 5 };
 int main() {
-    short bombs { 1 };
+    constexpr int& ref1 { g_x }; // 가능: 전역변수에 바인딩
 
-    const int& you {bombs};  // you는 int 객체에만 바인딩 가능, 컴파일러는 bombs를 암시적으로 int 로 변환, 즉 int 임시 객체 생김
-    --bombs; // 그렇기에 원래 short 자료형인 bombs를 감소시켜도 you에는 영향없음.
+    static int s_x { 6 };
+    constexpr int& ref2 { s_x }; // 가능: 정적 지역 변수에 바인딩
 
-    if (you) { // 즉, 그래서 true 값이 됨.
-        cout << "Bombs Away" << endl;
-    }
+    int x { 6 };
+    constexpr int& ref3 { x }; // 오류: 정적 저장 기간을 갖지 않는 객체에는 바인딩 불가.
+
     return 0;
 }

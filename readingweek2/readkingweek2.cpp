f76e23bc9ed5e1b2fee3b2a96eb448ef394f4b65
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-21 12:12:47 +0900

    참조 시 lvalue는 가능, rvalue 는 불가하나, const 가 있으면 lvalue rvalue 다 가능

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 int main() {
     int x = 10;
-    int& r = x;
+    int& r = x; // lvalue 는 이거 가능
     r = 20;
     cout << x << endl;
+
+    int& s = 5; // rvalue 는 불가
+    const int& t = 5; // const 는 lvalue rvalue 다 가능
     return 0;
 }

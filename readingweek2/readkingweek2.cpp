73391338ee4b1b5c9b3c4485b51db524908c2688
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-23 14:29:12 +0900

    14.1 객체지향프로그램의 장점, 클래스같은 걸 만들어서 속성 여러개를 한 클래스에 둘 수 있음.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
-#include <string>
+#include <string_view>
 using namespace std;
 
-void foo(int a, int& b, const string& c) {
-}
+struct Cat {
+    string_view name{ "cat" };
+    int numLegs{ 4 };
+};
+
+struct Dog {
+    string_view name{ "dog" };
+    int numLegs{ 4 };
+};
+
+struct Chicken {
+    string_view name{ "chicken" };
+    int numLegs{ 2 };
+};
+
 int main() {
-    int x { 5 };
-    string s { "Hello World!" };
-    
-    foo(5, x, s);
+    constexpr Cat animal;
+    cout << "A " << animal.name << " has " << animal.numLegs << " legs\n";
     return 0;
 }

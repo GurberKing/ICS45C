4247d14965bb88759b46596707357da1accce262
Junhyeok Park <JUNHYEP4@uci.edu>
2026-07-28 18:36:04 +0900

    14.4 const 객체의 데이터 멤버는 수정 불가.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -5,9 +5,17 @@ struct Date {
     int year {};
     int month {};
     int day {};
+
+    void incrementDay() {
+        ++day;
+    }
 };
 
 int main() {
     const Date today { 2020, 10, 14 }; // const 클래스 타입 객체
+
+    today.day += 1; // 오류: const 객체의 멤버 수정 불가.
+    today.incrementDay(); // 오류: 객체를 수정하는 멤버 함수 호출 불가.
+
     return 0;
 }

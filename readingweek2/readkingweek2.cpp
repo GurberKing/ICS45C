efaa4c10b42a3258c3f8f74802938cb5fbc82a40
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-03 13:01:31 +0900

    14.5 public으로 인해 멤버함수는 공개이므로 main에서 접근가능, 멤버는 비공개멤버에 접근가능.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Date { // class 멤버는 기본적으로 비공개 private, 다른멤버만 접근 가능.
-    int m_year {}; // 기본적으로 비공개
-    int m_month {}; // 기본적으로 비공개
-    int m_day {}; // 기본적으로 비공개
+class Date {
+private:
+    int m_year { 2020 };
+    int m_month { 10 };
+    int m_day { 14 };
 
-    void print() const { // 기본적으로 비공개
+public:
+    void print() const { // 멤버는 다른 비공개멤버에 접근할 수 있다.
         cout << m_year << '/' << m_month << '/' << m_day;
     }
 };
 
 int main() {
-    Date today { 2020, 10, 14 }; // 오류: 더 이상 집합체 초기화 사용불가.
-
-    // 일반 외부 코드에서는 비공개 멤버에 접근 불가.
-    today.m_day += 1; // 오류: m_day 멤버는 비공개.
-    today.print(); // 오류: print() 멤버 함수는 비공개.
+    Date d {};
+    d.print(); // 정상: main은 공개 멤버에 접근할 수 있다.
 
     return 0;
 }

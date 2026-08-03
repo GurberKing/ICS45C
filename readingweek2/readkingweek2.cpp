1837b4876ed82ba2a51258ca5fbb225cf1e21d1f
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-03 16:24:10 +0900

    14.9 집합체는 비공개 멤버를 가질 수 없다.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Point3d {
-private:
+class Foo { // 클래스는 기본적으로 private -> 집합체는 비공개멤버를 가질 수 없다.
     int m_x {};
     int m_y {};
-    int m_z {};
-public:
-    void setValues(const int x, const int y, const int z) {
-        m_x = x;
-        m_y = y;
-        m_z = z;
-    }
-
-    void print() {
-        cout << "<" << m_x << ", " << m_y << ", " << m_z << ">" << '\n';
-    }
-
-    bool isEqual(const Point3d& point) const{
-        return (m_x == point.m_x) && (m_y == point.m_y) && (m_z == point.m_z);
-    }
 };
 
 int main() {
-    Point3d point1{};
-    point1.setValues(1, 2, 3);
-    Point3d point2{};
-    point2.setValues(1, 2, 3);
-    std::cout << "point 1 and point 2 are" << (point1.isEqual(point2) ? "" : " not") << " equal\n";
-    Point3d point3{};
-    point3.setValues(3, 4, 5);
-    std::cout << "point 1 and point 3 are" << (point1.isEqual(point3) ? "" : " not") << " equal\n";
+    Foo foo { 6, 7 }; //오류
     return 0;
 }

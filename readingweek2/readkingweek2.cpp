21a47c0102435f8c2145d761be7f6dc933c89d5e
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-04 11:52:42 +0900

    14.9 생성자는 const면 안된다. 생성중인 객체를 초기화 할 수 있어야 하기 때문에. 일반적으로는 비상수 멤버 함수는 상수 객체애 대해 호출 할 수 없음. 그러나 객체가 생성 중일 때에는 const가 적용되지 않기에, const는 생성자 실행이 끝난 후에야 적용된다.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Foo {
+class Something {
+private:
+    int m_x{};
+
 public:
-    Foo(int x, int y) {
+    Something() { // 생성자는 반드시 non-const
+        m_x = 5; // non-const 생성자에서는 멤버 수정 가능.
     }
+
+    int getX() const { return m_x; } // const 멤버 함수
 };
 
 int main() {
-    Foo foo{ 'a', true }; // Foo(int, int) 생성자와 일치한다.
+    const Something s{}; // const 객체이며 non-const 생성자를 암시적으로 호출
+
+    cout << s.getX(); // 5 출력.
 
     return 0;
 }

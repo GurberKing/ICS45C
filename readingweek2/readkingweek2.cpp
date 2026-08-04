343582c867e8f9f7c9ff137dfd576a30638a48eb
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-04 11:25:41 +0900

    14.9 생성자 호출방법, 아직까지는 실제로 멤버함수 초기화 하지않음.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Foo { // 클래스는 기본적으로 private -> 집합체는 비공개멤버를 가질 수 없다.
+class Foo {
+private:
     int m_x {};
     int m_y {};
+public:
+    Foo(int x, int y) {
+        cout << "Foo(" << x << ", " << y << ") constructed\n";
+    }
+
+    void print() const {
+        cout << "Foo(" << m_x << ", " << m_y << ")\n";
+    }
 };
 
 int main() {
-    Foo foo { 6, 7 }; //오류
+    Foo foo { 6, 7 }; // Foo(int, int) 생성자 호출.
+    foo.print();
     return 0;
 }

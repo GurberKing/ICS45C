6a67c727654c41be00d092d4996354126dd1718a
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-04 11:30:04 +0900

    14.9 implicit type conversion: 인수 타입이 함수 매개변수 타입과 다르면, 컴파일러가 필요한 경우 암시적 타입 변환 한다.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Foo {
-private:
-    int m_x {};
-    int m_y {};
-public:
-    Foo(int x, int y) {
-        cout << "Foo(" << x << ", " << y << ") constructed\n";
-    }
-
-    void print() const {
-        cout << "Foo(" << m_x << ", " << m_y << ")\n";
-    }
-};
+void foo(int, int) {
+}
 
 int main() {
-    Foo foo { 6, 7 }; // Foo(int, int) 생성자 호출.
-    foo.print();
+    foo('a', true);
+
     return 0;
 }

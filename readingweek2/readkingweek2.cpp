9d3272d3217fb5cd96098dd73e5b3c40d61d71bb
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-04 13:12:19 +0900

    14.10 멤버 초기화 목록과 기본 멤버 초기화 값. 생성자와 멤버변수에 초기화 안되어 있으면 기본초기화 되어서 정의되지 않은 동작 발생.

--- readingweek2/readkingweek2.cpp
+++ readingweek2/readkingweek2.cpp
@@ -4,21 +4,23 @@ using namespace std;
 class Foo {
 private:
     int m_x {};
-    int m_y {};
+    int m_y { 2 };
+    int m_z;
 
 public:
-    Foo(int x, int y)
-        : m_x { x }, m_y { y } {
-            cout << "Foo(" << x << ", " << y << ") constructed\n";
+    Foo(int x)
+        : m_x { x }
+        {
+            cout << "Foo constructed\n";
         }
 
     void print() const {
-        cout << "Foo(" << m_x << ", " << m_y << ")\n";
+        cout << "Foo(" << m_x << ", " << m_y << ", " << m_z << ")\n";
     }
 };
 
 int main() {
-    Foo foo(6, 7);
+    Foo foo(6);
     foo.print();
 
     return 0;

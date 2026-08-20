31a87f37bc94c379bdb1027cb70f7996ae417344
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 18:59:22 +0900

    12.12 ReturnByReference는 참조되는 객체의 수명이 해당 참조를 반환하는 함수보다 길다는것을 보장해야한다. 그렇기에 static을 쓰는것. 이 예제는 static이 없기에 undefined error 발생.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 const string& getProgramName() { // return by reference to const
-    static const string s_programName = "Calculator";
+    const string s_programName = "Calculator";
     // static은 함수가 끝나는 순간 지역변수가 사라지는걸 방지한다.
     return s_programName;
 }

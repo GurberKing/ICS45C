5c8666f8b6f9e526acdb209e46732906f0ca71f1
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 17:14:04 +0900

    5.7 length(name)이 아니라, name.length()이다. length() 는 string의 멤버함수이기에. name 객체의 멤버함수를 호출하는 방식이다.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main() {
     string name = "Alex";
     cout << "name is " << name << "and length is " << name.length();
+    // length(name)이 아니라, name.length()이다.
+    // length() 는 string의 멤버함수이기에. name 객체의 멤버함수를 호출하는 방식이다.
     return 0;
 }

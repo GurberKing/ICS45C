486c86afc333516f17668c41ee3bcdd236be808d
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 18:47:15 +0900

    12.12 Return By Value, Return By Reference, Return By Reference To Const.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,15 +2,11 @@
 #include <string>
 using namespace std;
 int main() {
-    cout << "Enter your full name: ";
-    string name;
-    getline(cin >> ws, name);
-
-    cout << "Enter your age: ";
-    int age;
-    cin >> age;
-
-    int nameLen = static_cast<int>(name.length());
-    cout << "Your age + length of name is: " << age + nameLen;
+    string returnByValue();
+    // string의 복사본을 반환하는 함수, 비용이 크다.
+    string& returnByReference();
+    // string에 대한 참조를 반환. 저렴함.
+    const string& returnByReferenceToConst();
+    // string에 대한 const 참조를 반환함. 저렴함.
     return 0;
 }

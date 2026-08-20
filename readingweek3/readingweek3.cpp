94c418ebaf29aa5dfdc05aacaf3ba55ff4b116f2
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 19:31:34 +0900

    12.12 이럴 땐 id1과 id2 를 참조가 아닌 일반 변수로 만들어서 복사본을 만들면 해결.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 const int& getNextId() {
     static int s_x = 0; // static const가 아닌 정적지역변수만 사용.
-    ++s_x;
+    ++s_x; // ++ 이니깐 const를 쓸 수 없음.
     return s_x;
 }
 
 int main() {
-    const int& id1 = getNextId(); // 1로 예상 하지만 2로
-    const int& id2 = getNextId(); // 2로 예상 결과도 2.
+    const int id1 = getNextId();
+    const int id2 = getNextId();
     cout << id1 << id2 << '\n';
     return 0;
 }

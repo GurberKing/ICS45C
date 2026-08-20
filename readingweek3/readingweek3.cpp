aa614597692fe37841939941213195feacb233a2
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 18:17:11 +0900

    5.7 cout 시 접미사 없으면 c 스타일 문자열, s 붙으면 std::string 타입 문자열

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,6 +2,6 @@
 #include <string>
 using namespace std;
 int main() {
-    // string 은 값전달 안하는게 좋다. pass by value 시 큰 문자열 복사가 일어남.
-    // std::string_view 가 효율 측면에서 더 낫다.
+    cout << "foo\n";
+    cout << "goo\n"s;
 }

9f782031c158b61a9890110d1d02d9f6d024110a
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 19:02:40 +0900

    12.12 임시객체 또한 수명연장을 해줘야하는데, 그렇지 않으면 이번 예제처럼 undefined behavior 이 발생. 호출자에 있는 참조는 dangling reference 가 돼버림.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,13 +2,12 @@
 #include <string>
 using namespace std;
 
-const string& getProgramName() { // return by reference to const
-    const string s_programName = "Calculator";
-    // static은 함수가 끝나는 순간 지역변수가 사라지는걸 방지한다.
-    return s_programName;
+const int& returnByConstReference() {
+    return 5;
 }
 
 int main() {
-    cout << "This program is named " << getProgramName();
+    const int& ref = returnByConstReference();
+    cout << ref;
     return 0;
 }

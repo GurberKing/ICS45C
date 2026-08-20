3376a4c0e1854a06375a8212561915a577a49d99
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 17:17:16 +0900

    5.7 length() 는 부호가없는 정수값 unsigned int 를 반환하므로, 부호가 있게끔 하려면 static_cast<int>(name.length())를 해야함

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 int main() {
     string name = "Alex";
-    cout << "name is " << name << "and length is " << name.length();
+    cout << "name is " << name << "and length is " << name.length() << '\n';
     // length(name)이 아니라, name.length()이다.
     // length() 는 string의 멤버함수이기에. name 객체의 멤버함수를 호출하는 방식이다.
+
+    int length = static_cast<int>(name.length());
+    cout << "부호가 있는 정수 길이값은 " << length;
     return 0;
 }

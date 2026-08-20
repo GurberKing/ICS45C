e20d0035b95038a36616fe6289591732d84fa7cb
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 18:25:55 +0900

    5.7 문제 1, 내 풀이

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
-using namespace std::string_literals;
+using namespace std;
 int main() {
-    constexpr std::string name = "Alex"s; // compile error
+    cout << "Enter your full name: ";
+    string name;
+    getline(cin >> ws, name);
+
+    cout << "Enter your age: ";
+    int age;
+    cin >> age;
+    cout << "Your age + length of name is: " << age + name.length();
+    return 0;
 }

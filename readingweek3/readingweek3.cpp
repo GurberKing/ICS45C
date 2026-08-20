7baa998ca156300284dcb3978450982e45a8c546
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-20 17:22:40 +0900

    5.7 string 은 값전달 안하는게 좋다. pass by value 시 큰 문자열 복사가 일어남. std::string_view 가 효율 측면에서 더 낫다.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,12 +2,6 @@
 #include <string>
 using namespace std;
 int main() {
-    string name = "Alex";
-    cout << "name is " << name << "and length is " << name.length() << '\n';
-    // length(name)이 아니라, name.length()이다.
-    // length() 는 string의 멤버함수이기에. name 객체의 멤버함수를 호출하는 방식이다.
-
-    int length = static_cast<int>(name.length());
-    cout << "부호가 있는 정수 길이값은 " << length;
-    return 0;
+    // string 은 값전달 안하는게 좋다. pass by value 시 큰 문자열 복사가 일어남.
+    // std::string_view 가 효율 측면에서 더 낫다.
 }

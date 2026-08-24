a26dd4454492ad0064df4e781de4d03e79a2447a
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 18:22:31 +0900

    17.7 완전고전적인 방법으로 배열길이 구하기. sizeof() 이용.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <string>
-#include <iterator> // for size() ssize()
-#include <cstddef> // for size_t
 using namespace std;
 
-template <typename T, size_t N>
-constexpr size_t length(const T(&)[N]) noexcept {
-    return N;
-}
-
 int main() {
-    int array[] { 1, 1, 2, 3, 5, 8, 13, 21 };
-    cout << "The array has: " << length(array) << " elements";
+    int array[5] {};
+    cout << sizeof(array) / sizeof(array[0]);
     return 0;
 }

0990292845de64400f2fc28921152002837742d6
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 17:55:28 +0900

    17.7 sizeof함수로 배열 전체가 사용하는 바이트 수 변환.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,12 +2,8 @@
 #include <string>
 using namespace std;
 
-namespace ProgramData {
-    constexpr int square[5] { 1, 4, 5, 9, 2 };
-}
-
 int main() {
-    const int prime[5] { 2, 3, 5, 7, 11 };
-    prime[0] = 17; // error
+    const int prime[] { 2, 3, 5, 7, 11 };
+    cout << sizeof(prime);
     return 0;
 }

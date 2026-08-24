6fb509319731b1952306ada89beb91ea90a05c5a
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 18:19:32 +0900

    17.7 size() ssize() 없이 배열길이 구하는법, cstddef 사용하여 template 타입을 사용하여 들어갈 타입 추론 size_t 이용.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
-#include <iterator>
+#include <iterator> // for size() ssize()
+#include <cstddef> // for size_t
 using namespace std;
 
+template <typename T, size_t N>
+constexpr size_t length(const T(&)[N]) noexcept {
+    return N;
+}
+
 int main() {
-    const int prime[] { 2, 3, 5, 7, 11 };
-    cout << sizeof(prime) << '\n';
-    cout << size(prime) << '\n';
-    cout << ssize(prime);
+    int array[] { 1, 1, 2, 3, 5, 8, 13, 21 };
+    cout << "The array has: " << length(array) << " elements";
     return 0;
 }

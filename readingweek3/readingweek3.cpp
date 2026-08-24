6ec2507f9dbdcdec3009593074eefd772e1772f1
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 16:59:55 +0900

    14.11 default를 사용해서, 명시적으로 기본 설정된 기본 생성자 생성하기. default 를 하면 직접 기본생성자를 적을 필요 없다.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -7,10 +7,19 @@ private:
     int m_x{};
     int m_y{};
 
-    // 선언된 생성자가 없음.
+public:
+    Foo() = default;
+
+    Foo(int x, int y)
+    :m_x{ x }
+    ,m_y{ y }
+    {
+        cout << "Foo(" << m_x << ", " << m_y << ") constructed";
+    }
 };
 
 int main() {
     Foo foo{};
+    Foo foo1(2, 3);
     return 0;
 }

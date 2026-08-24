84d866d1001d566f41479159b6ba5c2909ed4b99
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 18:06:21 +0900

    17.7 size함수는 배열의 unsigned int 길이, ssize함수는 배열의 signed int 길이.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include <iterator>
 using namespace std;
 
 int main() {
     const int prime[] { 2, 3, 5, 7, 11 };
-    cout << sizeof(prime);
+    cout << sizeof(prime) << '\n';
+    cout << size(prime) << '\n';
+    cout << ssize(prime);
     return 0;
 }

8d3f7aa1900724fbf4595f317637820e19f4dd3b
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 18:30:26 +0900

    17.7 대신 배열의 내용을 한꺼번에 바꾸려면, <algorithm>을 사용하여 copy 할 수 있음.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
+#include <algorithm> // for copy
 using namespace std;
-
 int main() {
     int arr[] { 1, 2, 3 };
-    arr[0] = 4; // 가능
-    arr = { 5, 6, 7 }; // 에러
+    int src[] { 5, 6, 7 };
+
+    copy(begin(src), end(src), begin(arr));
+    // src 배열의 내용을 arr로 복사.
+    cout << arr[0] << '\n';
     return 0;
 }

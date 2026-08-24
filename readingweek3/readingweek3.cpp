b9ab3bc21dee5f5f8c89fbd363af06213818f33f
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 17:51:09 +0900

    17.7 길이가 0인 c스타일 배열은 오류.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
-    auto square[5] { 1, 4, 5, 6, 7 }; // c스타일 배열은 auto 불가.
+    const int prime1[5] { 2, 3, 5, 7, 9 };
+    const int prime2[] { 2, 3, 5, 6 ,2 }; // 컴파일러가 배열 길이가 5라고 추론.
+
+    int bad[] {}; // 에러발생. 초기화 값이 없기에 컴파일러가 배열 길이가 0이라고 추론. 하지만, 길이가 0인 c스타일 배열은 허용하지 않으므로 오류.
     return 0;
 }

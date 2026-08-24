91cc6ae4a5c4be10a6c560b8d9bc2d0af660a0ca
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-24 17:54:15 +0900

    17.7 const constexpr 배열선언가능. 대신 초기화 이후 배열원소 값 변경불가.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,10 +2,12 @@
 #include <string>
 using namespace std;
 
-int main() {
-    const int prime1[5] { 2, 3, 5, 7, 9 };
-    const int prime2[] { 2, 3, 5, 6 ,2 }; // 컴파일러가 배열 길이가 5라고 추론.
+namespace ProgramData {
+    constexpr int square[5] { 1, 4, 5, 9, 2 };
+}
 
-    int bad[] {}; // 에러발생. 초기화 값이 없기에 컴파일러가 배열 길이가 0이라고 추론. 하지만, 길이가 0인 c스타일 배열은 허용하지 않으므로 오류.
+int main() {
+    const int prime[5] { 2, 3, 5, 7, 11 };
+    prime[0] = 17; // error
     return 0;
 }

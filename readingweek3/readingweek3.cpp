52316a466b13a60d4845647b0b76e2a3377e2b9c
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-25 21:32:38 +0900

    17.7 문제3번 정답.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
-int main() {
-    cout << "Enter a single digit integer, or -1 to quit: ";
-    int num;
-    cin >> num;
-    constexpr int arr[] = { 0, 1, 4, 9};
-    if (num == -1) {
-        break;
-    }
 
-    for (int i = 0; i < 4; i++) {
-        if (arr[i] == num) {
-            cout << num << "is a perfect square.\n";
+int main()
+{
+    constexpr int arr[] { 0, 1, 4, 9 };
+
+    while (true)
+    {
+        cout << "Enter a single digit integer, or -1 to quit: ";
+
+        int num{};
+        cin >> num;
+
+        if (num == -1)
+        {
+            break;
+        }
+
+        bool found{ false };
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (arr[i] == num)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            cout << num << " is a perfect square.\n";
+        }
+        else
+        {
+            cout << num << " is not a perfect square.\n";
         }
     }
+
+    cout << "Bye\n";
+
     return 0;
 }

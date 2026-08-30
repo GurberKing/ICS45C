47ed526fac41483251803804b60fc1c1d42686af
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-30 17:53:03 +0900

    17.9 포인터 산술, + - 다 됨.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,17 +2,10 @@
 #include <string>
 using namespace std;
 
-void printElementZero(int arr[]) {    // arr에 최소 3개의 원소가 있다는것을 어떻게 보장?
-    cout << arr[2];
-}
 int main() {
-    int a[] { 3, 2, 1 };
-    printElementZero(a); // 정상
+    int x {};
+    const int* ptr = &x;
 
-    int b[] { 7, 6 };
-    printElementZero(b); // 정의되지 않은 동작 발생
-
-    int c[] { 9 };
-    printElementZero(c); // 정의되지 않은 동작 발생
+    cout << ptr << ' ' << ptr + 1 << ' ' << ptr + 2 << '\n';
     return 0;
 }

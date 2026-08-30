536b88079cc53d2bc7a75fca7762e6e2ad66151d
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-30 17:08:59 +0900

    17.8 배열붕괴, 배열의 포인터로도 []로 접근가능.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,45 +1,9 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
-int main()
-{
-    constexpr int arr[] { 0, 1, 4, 9 };
-
-    while (true)
-    {
-        cout << "Enter a single digit integer, or -1 to quit: ";
-
-        int num{};
-        cin >> num;
-
-        if (num == -1)
-        {
-            break;
-        }
-
-        bool found{ false };
-
-        for (int i = 0; i < 4; i++)
-        {
-            if (arr[i] == num)
-            {
-                found = true;
-                break;
-            }
-        }
-
-        if (found)
-        {
-            cout << num << " is a perfect square.\n";
-        }
-        else
-        {
-            cout << num << " is not a perfect square.\n";
-        }
-    }
-
-    cout << "Bye\n";
-
+int main() {
+    const int arr[] = { 9, 7, 5, 3, 1 };
+    const int* ptr = arr;
+    cout << ptr[2];
     return 0;
 }

ed25767c2ba6df4d5eb55253c0ddf2dc62bb10df
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-30 17:14:47 +0900

    17.8 const int 자료형의 배열에서 const int* 포인터로 붕괴.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+void printElementZero(const int* arr) {
+    cout << arr[0];
+}
 int main() {
-    const int arr[] = { 9, 7, 5, 3, 1 };
-    const int* ptr = arr;
-    cout << ptr[2];
+    const int prime[] { 2, 3, 5, 7, 11 };
+    const int squares[] { 1, 4, 9, 16, 25, 36, 49, 64, 81 };
+    printElementZero(prime);
+    printElementZero(squares);
     return 0;
 }

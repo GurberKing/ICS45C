d67abb855eb09f22670f91ee58e52bdef772ce2f
Junhyeok Park <JUNHYEP4@uci.edu>
2026-08-30 17:17:35 +0900

    17.8 함수 배개변수에 배열 자료형 넣을 때, int* arr 보다는 int arr[] 이게 더 낫다. 어차피 똑같이 적용되기에.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -2,13 +2,13 @@
 #include <string>
 using namespace std;
 
-void printElementZero(const int* arr) {
+void printElementZero(const int arr[]) {    // const int* 과 동일하게 처리됨.
     cout << arr[0];
 }
 int main() {
     const int prime[] { 2, 3, 5, 7, 11 };
     const int squares[] { 1, 4, 9, 16, 25, 36, 49, 64, 81 };
-    printElementZero(prime);
-    printElementZero(squares);
+    printElementZero(prime);    // prime은 포인터로 붕괴.
+    printElementZero(squares);  // sxquares는 포인터로 붕괴.
     return 0;
 }

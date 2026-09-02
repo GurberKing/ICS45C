c70a0ac9dc79e59f38def3bec6d91b121effa4ac
Junhyeok Park <JUNHYEP4@uci.edu>
2026-09-02 17:11:40 +0900

    17.9 auto e for문을 사용한 포인터산술로 배열 순환.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 
 int main() {
-    constexpr int arr[] { 9, 7, 5, 3, 1 };
-    const int* begin{ arr }; // 시작원소 가리킴
-    const int* end{ arr + size(arr) }; // 마지막원소의 바로 다음을 가리킴
+    constexpr int arr[] {9, 7, 5, 3, 1};
 
-    for (; begin != end; ++begin) { // begin부터 end 직전까지 반복
-        cout << *begin << ' '; // 현재 원소 얻기 위해 역참조.
+    for (auto e : arr) {
+        cout << e << ' ';
     }
     return 0;
 }

9c1058db4340b48d98651925a815b16f7c3f03f9
Junhyeok Park <JUNHYEP4@uci.edu>
2026-09-21 16:15:54 -0700

    12.10 대신 cstring라이브러리 include해서 strlen을 사용하면 문자열의 정확한 길이와, 포인터로 decay된 문자열의 길이도 정확하게 나온다. 단점은 느림.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 #include <iterator>
+#include <cstring>
 using namespace std;
 
 int main() {
     char str[255]{ "string" };
-    cout << "length: " << size(str) << '\n';
+    cout << "length: " << strlen(str) << '\n';
 
     char *ptr { str };
-    cout << "length: " << size(ptr) << '\n';
+    cout << "length: " << strlen(ptr) << '\n';
     return 0;
 }

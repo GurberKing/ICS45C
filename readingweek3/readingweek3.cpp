e61de696f74142701878af52bd83ed1b2347f3ae
Junhyeok Park <JUNHYEP4@uci.edu>
2026-09-21 16:09:00 -0700

    12.10 c스타일 문자열은 char [] 배열 형식이므로, 배열 원소의 몇 번째 원소 바꿀것인지 가능.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 
 int main() {
-    char rolls[255] {}; // 문자 254개 + 널문자 1개 넣을 수 있을만큼 큰 배열선언
-    cout << "Enter your rolls: ";
-    cin.getline(rolls, size(rolls));
-    cout << "You entered: " << rolls << '\n';
+    char str[] {"string"};
+    cout << str << '\n';
+
+    str[1] = 'p';
+    cout << str << '\n';
     return 0;
 }

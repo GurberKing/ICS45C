900cb70f9a76a85b54320a5efcfe3a1a6fa93e31
Junhyeok Park <JUNHYEP4@uci.edu>
2026-09-21 18:17:01 -0700

    21.4 연산자 오버로딩을 통한 operator<< 작성, 클래스의 멤버변수도 << 으로 사용가능하게끔.

--- readingweek3/readingweek3.cpp
+++ readingweek3/readingweek3.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <string>
-using namespace std;
-void printCStringBackwards(const char str[]) {
-    const char* ptr = str;
+class Point {
+private:
+    double m_x{};
+    double m_y{};
+    double m_z{};
 
-    while (*ptr != '\0') {
-        ptr++;
-    }
+public:
+    Point(double x=0.0, double y=0.0, double z= 0.0)
+        : m_x{x}, m_y{y}, m_z{z} {}
+    
+    friend std::ostream& operator<< (std::ostream& out, const Point& point);
+};
 
-    while (ptr != str) {
-        --ptr;
-        cout << *ptr;
-    }
+std::ostream& operator<< (std::ostream& out, const Point& point) {
+    //operator<< 는 Point클래스의 friend 이므로, Point의 멤버에 접근가능.
+    out << "Point(" << point.m_x << ", " << point.m_y << ", " << point.m_z << ")";
+    return out; // operator<< 호출을 연결 할 수 있도록 std::ostream을 반환.
 }
 
 int main() {
-    char name[] { "Hello, World!" };
-    printCStringBackwards(name);
+    const Point point1 { 2.0, 3.0, 4.0 };
+    std::cout << point1 << '\n';
     return 0;
 }
